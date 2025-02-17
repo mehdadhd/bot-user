398358e98dcb6f90998d79ad6174c9cf43e483cd
mehdadhd <33844722+mehdadhd@users.noreply.github.com>
2025-02-17 12:00:15 +0330

    2

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ require("dotenv").config();
 
 // اتصال به MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Error connecting to MongoDB:", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // مدل کاربر
 const User = mongoose.model(
